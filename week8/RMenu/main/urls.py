edc97ae47d8f07d05247e6f35fcf59846c45fce0
Symbat <sbashkeyeva@gmail.com>
2018-11-01 17:00:03 +0600

    up to date

--- week8/RMenu/main/urls.py
+++ week8/RMenu/main/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns=[
     path('', views.home, name='home'),
+    path('rests/',views.rests, name='rests'),
+    path('detailed_rest/<int:id>/',views.detailed_rest, name='detailed_rest'),
+    path('add_rest/', views.add_rest,name='add_rest'),
+    path('delete_rest/<int:id>/', views.delete_rest, name='delete_rest'),
+    path('update_rest/<int:id>/', views.update_rest, name='update_rest'),
+    path('delete_all_rest/', views.delete_all_rest, name='delete_all_rest')
+
 ]
--- week8/RMenu/main/views.py
+++ week8/RMenu/main/views.py
@@ -1,9 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Restaurant
-def home(request):
-    rests=Restaurant.objects.all()
-    context={
-        'rests':rests
-    }
-    return render(request,'home.html',context)
+from django.contrib.auth.decorators import login_required
+from .forms import RestaurantForm
 
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def rests(request):
+    rests = Restaurant.objects.all()
+    context = {'rests': rests}
+    return render(request, 'rests.html', context)
+
+
+def detailed_rest(request, id):
+    rest = Restaurant.objects.get(pk=id)
+    context = {'rest': rest}
+    return render(request, 'detailed_rest.html', context)
+
+
+def add_rest(request):
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rests')
+    else:
+        form = RestaurantForm
+    context = {'form': form}
+
+    return render(request, 'add_rest.html', context)
+
+
+def delete_rest(request, id):
+    rest = Restaurant.objects.get(pk=id)
+    rest.delete()
+    return redirect('rests')
+
+
+def delete_all_rest(request):
+    rests = Restaurant.objects.all()
+    rests.delete()
+    return redirect('rests')
+
+
+def update_rest(request, id):
+    updated_rest = get_object_or_404(Restaurant, pk=id)
+    form = RestaurantForm(request.POST or None, instance=updated_rest)
+    if form.is_valid():
+        form.save()
+        return redirect('rests')
+    return render(request, 'add_rest.html', {'form': form})
